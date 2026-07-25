e5462e075b6d43790771ca87101eea36314cbc96
Esther Akindele <estherakindele09@gmail.com>
2026-07-25 22:50:06 +0200

    Configure email service

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,47 +1,26 @@
 const nodemailer = require("nodemailer");
 
+console.log("EMAIL_HOST =", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT =", process.env.EMAIL_PORT);
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+
 const transporter = nodemailer.createTransport({
-
-    host: "smtp.gmail.com",
-    port: 587,
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
     secure: false,
-
     auth: {
-
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
-
-    },
-
-    tls: {
-        rejectUnauthorized: false
     }
-
-});
-
-transporter.verify((error) => {
-
-    if (error) {
-        console.error("Email configuration error:", error);
-    } else {
-        console.log("Email server is ready.");
-    }
-
 });
 
 async function sendEmail(to, subject, html) {
-
-    return await transporter.sendMail({
-
+    return transporter.sendMail({
         from: `"LiquorStore POS" <${process.env.EMAIL_USER}>`,
         to,
         subject,
         html
-
     });
-
 }
 
-module.exports = {
-    sendEmail
-};
+module.exports = { sendEmail };
