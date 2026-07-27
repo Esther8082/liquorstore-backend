d97c2a6b03cb74fb5c5120109ff41d11d0a8832f
Esther Akindele <estherakindele09@gmail.com>
2026-07-27 06:17:48 +0200

    Use Brevo Email API

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,53 +1,57 @@
-const nodemailer = require("nodemailer");
-
-console.log("EMAIL_HOST =", process.env.EMAIL_HOST);
-console.log("EMAIL_PORT =", process.env.EMAIL_PORT);
-console.log("EMAIL_USER =", process.env.EMAIL_USER);
-
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false,
-    requireTLS: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    },
-    connectionTimeout: 30000,
-    greetingTimeout: 30000,
-    socketTimeout: 30000
-});
-
-// Verify SMTP when the server starts
-transporter.verify((error, success) => {
-
-    if (error) {
-
-        console.error("SMTP VERIFY FAILED");
-        console.error(error);
-
-    } else {
-
-        console.log("SMTP VERIFY SUCCESS");
-
-    }
-
-});
+const axios = require("axios");
 
 async function sendEmail(to, subject, html) {
 
-    return transporter.sendMail({
+    const response = await axios.post(
 
-        from: `"LiquorStore POS" <${process.env.EMAIL_USER}>`,
+        "https://api.brevo.com/v3/smtp/email",
 
-        to,
+        {
 
-        subject,
+            sender: {
 
-        html
+                name: "LiquorStore POS",
 
-    });
+                email: "estherakindele09@gmail.com"
+
+            },
+
+            to: [
+
+                {
+
+                    email: to
+
+                }
+
+            ],
+
+            subject,
+
+            htmlContent: html
+
+        },
+
+        {
+
+            headers: {
+
+                "api-key": process.env.BREVO_API_KEY,
+
+                "Content-Type": "application/json"
+
+            }
+
+        }
+
+    );
+
+    return response.data;
 
 }
 
-module.exports = { sendEmail };
+module.exports = {
+
+    sendEmail
+
+};
